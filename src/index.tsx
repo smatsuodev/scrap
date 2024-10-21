f7f5bad26d790554fae44729a30ec7ddb65c21ab
smatsuodev <git@smatsuo.dev>
2024-10-21 21:43:25 +0900

    pagesにデプロイする用に作り直し

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import { Hono } from 'hono'
+import { renderer } from './renderer'
 
 const app = new Hono()
 
+app.use(renderer)
+
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.render(<h1>Hello!</h1>)
 })
 
 export default app
