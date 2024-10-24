5e0629b47bcdef3a274f47d3e3e4414640930bef
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-10-24 22:56:39 +0900

    Fragment に編集ボタンを置く

--- src/client/component/FragmentViewer.tsx
+++ src/client/component/FragmentViewer.tsx
@@ -1,5 +1,6 @@
 import type { Fragment } from '@/client/model/fragment'
 import { Card, Text } from '@mantine/core'
+import { IconEdit } from '@tabler/icons-react'
 
 interface FragmentViewerProps {
   fragment: Fragment
@@ -9,6 +10,7 @@ export function FragmentViewer({ fragment }: FragmentViewerProps) {
   return (
     <Card withBorder className='whitespace-pre-wrap'>
       <Text>{fragment.content}</Text>
+      <IconEdit />
     </Card>
   )
 }
