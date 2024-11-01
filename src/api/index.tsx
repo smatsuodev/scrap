e9b25439281d7e37751ec0e7f071d6e27b3d6577
smatsuodev <git@smatsuo.dev>
2024-11-01 12:10:21 +0900

    ulidのallowInsecureをtrueに変更

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -6,6 +6,7 @@ import { type DrizzleD1Database, drizzle } from 'drizzle-orm/d1'
 import { Hono } from 'hono'
 import { createMiddleware } from 'hono/factory'
 import { renderToString } from 'react-dom/server'
+import { detectPrng, factory } from 'ulid'
 import { z } from 'zod'
 
 const drizzleMiddleware = createMiddleware(async (c, next) => {
@@ -68,7 +69,10 @@ const api = new Hono<Env>()
     ),
     async (c) => {
       const { title } = c.req.valid('json')
-      const { ulid } = await import('ulid')
+      // FIXME: ulidライブラリはdynamic requireでPRNGを読み込むが、Cloudflare Pagesでは動作しないため、Math.randomを使うことを許している
+      // Math.randomは安全ではないので対処法をいつか考える
+      const prng = detectPrng(true)
+      const ulid = factory(prng)
       const scrap = {
         id: ulid(Date.now()),
         title,
