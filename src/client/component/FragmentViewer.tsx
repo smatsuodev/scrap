a1991c0dbf12ca055f0c921ab7b4d183f4a06397
smatsuodev <git@smatsuo.dev>
2024-11-02 11:37:07 +0900

    react-remarkをreact-markdownにリプレイス

--- src/client/component/FragmentViewer.tsx
+++ src/client/component/FragmentViewer.tsx
@@ -7,7 +7,7 @@ import {
   Title,
   type TitleProps,
 } from '@mantine/core'
-import { Remark } from 'react-remark'
+import Markdown from 'react-markdown'
 
 interface FragmentViewerProps {
   fragment: Fragment
@@ -16,22 +16,20 @@ interface FragmentViewerProps {
 export function FragmentViewer({ fragment }: FragmentViewerProps) {
   return (
     <Card withBorder className='whitespace-pre-wrap'>
-      <Remark
-        rehypeReactOptions={{
-          components: {
-            h1: (props: TitleProps) => <Title order={1} {...props} />,
-            h2: (props: TitleProps) => <Title order={2} {...props} />,
-            h3: (props: TitleProps) => <Title order={3} {...props} />,
-            h4: (props: TitleProps) => <Title order={4} {...props} />,
-            h5: (props: TitleProps) => <Title order={5} {...props} />,
-            h6: (props: TitleProps) => <Title order={6} {...props} />,
-            ul: (props: ListProps) => <List listStyleType='disc' {...props} />,
-            li: (props: ListItemProps) => <List.Item {...props} />,
-          },
+      <Markdown
+        components={{
+          h1: (props: TitleProps) => <Title order={1} {...props} />,
+          h2: (props: TitleProps) => <Title order={2} {...props} />,
+          h3: (props: TitleProps) => <Title order={3} {...props} />,
+          h4: (props: TitleProps) => <Title order={4} {...props} />,
+          h5: (props: TitleProps) => <Title order={5} {...props} />,
+          h6: (props: TitleProps) => <Title order={6} {...props} />,
+          ul: (props: ListProps) => <List listStyleType='disc' {...props} />,
+          li: (props: ListItemProps) => <List.Item {...props} />,
         }}
       >
         {fragment.content}
-      </Remark>
+      </Markdown>
     </Card>
   )
 }
