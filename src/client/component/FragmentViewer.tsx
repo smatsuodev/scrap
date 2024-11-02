bc536184780f48ea5d3efd049f732400f63a8314
smatsuodev <git@smatsuo.dev>
2024-11-02 11:20:22 +0900

    markdownのうち、見出しと箇条書きに対応

--- src/client/component/FragmentViewer.tsx
+++ src/client/component/FragmentViewer.tsx
@@ -1,5 +1,13 @@
 import type { Fragment } from '@/client/model/fragment'
-import { Card, Text } from '@mantine/core'
+import {
+  Card,
+  List,
+  type ListItemProps,
+  type ListProps,
+  Title,
+  type TitleProps,
+} from '@mantine/core'
+import { Remark } from 'react-remark'
 
 interface FragmentViewerProps {
   fragment: Fragment
@@ -8,7 +16,22 @@ interface FragmentViewerProps {
 export function FragmentViewer({ fragment }: FragmentViewerProps) {
   return (
     <Card withBorder className='whitespace-pre-wrap'>
-      <Text>{fragment.content}</Text>
+      <Remark
+        rehypeReactOptions={{
+          components: {
+            h1: (props: TitleProps) => <Title order={1} {...props} />,
+            h2: (props: TitleProps) => <Title order={2} {...props} />,
+            h3: (props: TitleProps) => <Title order={3} {...props} />,
+            h4: (props: TitleProps) => <Title order={4} {...props} />,
+            h5: (props: TitleProps) => <Title order={5} {...props} />,
+            h6: (props: TitleProps) => <Title order={6} {...props} />,
+            ul: (props: ListProps) => <List listStyleType='disc' {...props} />,
+            li: (props: ListItemProps) => <List.Item {...props} />,
+          },
+        }}
+      >
+        {fragment.content}
+      </Remark>
     </Card>
   )
 }
