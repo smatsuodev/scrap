1d923120e456d77a9505ceb03308815ad8673579
smatsuodev <git@smatsuo.dev>
2024-11-06 23:11:27 +0900

    markdownの要素間のmarginをcss moduleで当てるように変更し、箇条書きの中にあるpタグにはmargin-bottomを設定しないように変更

--- src/client/component/FragmentViewer.tsx
+++ src/client/component/FragmentViewer.tsx
@@ -21,15 +21,15 @@ export function FragmentViewer({ fragment }: FragmentViewerProps) {
     <Card withBorder className={classes.markdown}>
       <Markdown
         components={{
-          h1: (props: TitleProps) => <Title mb='1rem' order={1} {...props} />,
-          h2: (props: TitleProps) => <Title mb='1rem' order={2} {...props} />,
-          h3: (props: TitleProps) => <Title mb='1rem' order={3} {...props} />,
-          h4: (props: TitleProps) => <Title mb='1rem' order={4} {...props} />,
-          h5: (props: TitleProps) => <Title mb='1rem' order={5} {...props} />,
-          h6: (props: TitleProps) => <Title mb='1rem' order={6} {...props} />,
+          h1: (props: TitleProps) => <Title order={1} {...props} />,
+          h2: (props: TitleProps) => <Title order={2} {...props} />,
+          h3: (props: TitleProps) => <Title order={3} {...props} />,
+          h4: (props: TitleProps) => <Title order={4} {...props} />,
+          h5: (props: TitleProps) => <Title order={5} {...props} />,
+          h6: (props: TitleProps) => <Title order={6} {...props} />,
           ul: (props: ListProps) => <List listStyleType='disc' {...props} />,
           li: (props: ListItemProps) => <List.Item {...props} />,
-          p: (props: TextProps) => <Text mb='1rem' {...props} />,
+          p: (props: TextProps) => <Text {...props} />,
         }}
       >
         {fragment.content}
