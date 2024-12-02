f23bcaf17c40cdda065c3b1c9868d43a9d9fc4c3
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-12-03 01:37:36 +0900

    session は model に定義

--- src/server/repository/session.ts
+++ src/server/repository/session.ts
@@ -1,15 +1,10 @@
-import type { Brand } from '@/common/brand'
+import {
+  type Session,
+  type SessionId,
+  SessionSchema,
+} from '@/common/model/session'
 import type { UserId } from '@/common/model/user'
 import { ulid } from 'ulidx'
-import { z } from 'zod'
-
-export type SessionId = Brand<string, 'SessionId'>
-
-const SessionSchema = z.object({
-  id: z.string().transform((v) => v as SessionId),
-  userId: z.string().transform((v) => v as UserId),
-})
-export type Session = z.infer<typeof SessionSchema>
 
 export interface ISessionRepository {
   createSession(userId: UserId): Promise<Session>
