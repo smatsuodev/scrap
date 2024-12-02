143b9c2eab692f490cdc81a3fa15a4683c95d847
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-12-03 02:08:11 +0900

    動作確認用の register の仮実装

--- src/server/routes/auth.ts
+++ src/server/routes/auth.ts
@@ -1,20 +1,22 @@
 import type { UserId } from '@/common/model/user'
+import * as schema from '@/server/db/schema'
 import type { AppEnv } from '@/server/env'
 import { zValidator } from '@hono/zod-validator'
 import argon2 from 'argon2'
 import { Hono } from 'hono'
 import { z } from 'zod'
 
-const auth = new Hono<AppEnv>().basePath('/auth').post(
-  '/login',
-  zValidator(
-    'json',
-    z.object({
-      userId: z.string().transform((v) => v as UserId),
-      password: z.string(),
-    }),
-  ),
-  async (c) => {
+const authInputValidator = zValidator(
+  'json',
+  z.object({
+    userId: z.string().transform((v) => v as UserId),
+    password: z.string(),
+  }),
+)
+
+const auth = new Hono<AppEnv>()
+  .basePath('/auth')
+  .post('/login', authInputValidator, async (c) => {
     const { userId, password } = c.req.valid('json')
     const user = await c.var.db.query.users.findFirst({
       where: (users, { eq }) => eq(users.id, userId),
@@ -31,7 +33,16 @@ const auth = new Hono<AppEnv>().basePath('/auth').post(
     const session = await c.var.sessionRepository.createSession(userId)
 
     return c.json(session)
-  },
-)
+  })
+  .post('/register', authInputValidator, async (c) => {
+    // TODO: これは動作確認用の仮実装
+    const { userId, password } = c.req.valid('json')
+    const hash = await argon2.hash(password)
+    await c.var.db.insert(schema.users).values({
+      id: userId,
+      password: hash,
+    })
+    return c.body(null, 201)
+  })
 
 export default auth
