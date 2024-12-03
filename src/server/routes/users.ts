f94209f3519f6414e43a9c4047ba71452742c8f7
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-12-04 00:43:42 +0900

    GET /users/me

---
+++ src/server/routes/users.ts
@@ -0,0 +1,25 @@
+import type { User } from '@/common/model/user'
+import type { AppEnv } from '@/server/env'
+import { sessionAuthMiddleware } from '@/server/middleware/sessionAuth'
+import { Hono } from 'hono'
+
+const users = new Hono<AppEnv>()
+  .basePath('/users')
+  .get('/me', sessionAuthMiddleware, async (c) => {
+    const session = c.var.session
+    if (!session) {
+      return c.body(null, 401)
+    }
+
+    const user = await c.var.db.query.users.findFirst({
+      where: (users, { eq }) => eq(users.id, session.userId),
+      columns: { id: true },
+    })
+    if (!user) {
+      return c.body(null, 401)
+    }
+
+    return c.json({ id: user.id } satisfies User)
+  })
+
+export default users
