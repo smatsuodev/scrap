b2eea282d87f7523dd2b170c66965d989e9b0b33
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-12-06 02:19:15 +0900

    fragment 作成時に author も保存

--- src/common/model/fragment.ts
+++ src/common/model/fragment.ts
@@ -1,9 +1,12 @@
 import type { Brand } from '@/common/brand'
+import type { UserId } from '@/common/model/user'
 
 export type FragmentId = Brand<number, 'FragmentId'>
 
 export interface Fragment {
   id: FragmentId
+  scrapId: string
+  authorId: UserId
   content: string
 }
 
