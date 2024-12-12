7412ccab3f45a2ee044f438259ec29e4c99255e6
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-12-13 03:20:13 +0900

    route の実装

--- src/server/routes/auth/index.ts
+++ src/server/routes/auth/index.ts
@@ -1,6 +1,10 @@
+import passkeyAuth from '@/server/routes/auth/passkey'
 import passwordAuth from '@/server/routes/auth/password'
 import { Hono } from 'hono'
 
-const auth = new Hono().basePath('/auth').route('/', passwordAuth)
+const auth = new Hono()
+  .basePath('/auth')
+  .route('/', passwordAuth)
+  .route('/passkey', passkeyAuth)
 
 export default auth
